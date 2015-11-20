dd75a7f97d46ab4dbaf7e273647ca1805e9f1f25
yanchao <yanchao.1003@gmail.com>
2015-11-20 16:10:22 +0800

    alert user

--- SringRids/src/main/java/com/yc/bean/User.java
+++ SringRids/src/main/java/com/yc/bean/User.java
@@ -3,15 +3,11 @@ package com.yc.bean;
 import java.io.Serializable;
 
 public class User implements Serializable {
-
     private static final long serialVersionUID = -6011241820070393952L;
-
     private String            id;
-
     private String            name;
-
     private String            password;
-
+    
     public User() {
 
     }
